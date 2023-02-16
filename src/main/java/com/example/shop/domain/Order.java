79518373209488ff4d2b14b6d7531aee884ed7db
dltldud2kr <dltldud2kr@gmail.com>
2023-02-16 16:43:56 +0900

    2023-02-16

--- src/main/java/com/example/shop/domain/Order.java
+++ src/main/java/com/example/shop/domain/Order.java
@@ -22,10 +22,10 @@ public class Order {
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -34,4 +34,29 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status; //주문 상태 [ORDER, CANCEL]
 
+
+    //== 연관관계 편의 메서드 ==//
+    public void SetMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+        // Member member = new Member();
+        // Order order = new Order();
+        //
+        // member.getOrders().add(order);
+        // order.setMember(member);
+
+        // 주석처리한 코드가 위의 SetMember 메서드 하나로 order.setMember(member); 이거 하나로 줄음
+
+        // -- 이 포함 아래 메서드들 전부 양방향일 때 쓰면 좋음.
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
