c33f5938296171c1fbd05c02662d77598e2aa522
dltldud2kr <dltldud2kr@gmail.com>
2023-03-02 23:20:40 +0900

    2023-03-02 회원 도메인 개발 상품 도메인 개발

--- src/main/java/com/example/shop/domain/item/Item.java
+++ src/main/java/com/example/shop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.example.shop.domain.item;
 
 import com.example.shop.domain.Category;
+import com.example.shop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,4 +27,25 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비지니스 로직==//
+
+    //데이터(stockQuantity)를 가지고 있는 쪽에 비지니스 메서드가 있는게 가장 좋음.
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
